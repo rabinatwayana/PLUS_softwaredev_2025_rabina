7569af61adc5fb1868f95b62c1be63c9531d36cf
rabinatwayana <rabinatwayana123@gmail.com>
2025-03-24 11:13:43 +0100

    add: say_hello python script

---
+++ b_python_script.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+# start
+print(sys.path)
+help("modules")
+days_of_the_week = 7
+def say_hello(recipient):
+    print("Hello, world! Hello {} ".format(recipient))
+    return recipient
